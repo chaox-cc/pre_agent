8dcc1a975aa97baa91c844b6b034bfc5a8c0ddce
hzc <huangzhch@deepinsighttech.com>
2026-08-18 09:27:10 +0800

    feat: ragagent

--- src/pre_agent/main.py
+++ src/pre_agent/main.py
@@ -1,29 +1,45 @@
-# import src.pre_agent.HelloAgentsLLM as HelloAgentsLLM
-import pre_agent.search as search
-import pre_agent.ToolExecutor as ToolExecutor
+from dotenv import load_dotenv
+from pre_agent.tools import search, ToolExecutor
+from pre_agent.ReActAgent import ReActAgent
+from pre_agent.HelloAgentsLLM import HelloAgentsLLM
+
+load_dotenv()
+
+# def main():
+#     # 1. 初始化工具执行器
+#     toolExecutor = ToolExecutor()
+
+#     # 2. 注册我们的实战搜索工具
+#     search_description = "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中找不到的信息时，应使用此工具。"
+#     toolExecutor.registerTool("Search", search_description, search)
+    
+#     # 3. 打印可用的工具
+#     print("\n--- 可用的工具 ---")
+#     print(toolExecutor.getAvailableTools())
+
+#     # 4. 智能体的Action调用，这次我们问一个实时性的问题
+#     print("\n--- 执行 Action: Search['英伟达最新的GPU型号是什么'] ---")
+#     tool_name = "Search"
+#     tool_input = "英伟达最新的GPU型号是什么"
+
+#     tool_function = toolExecutor.getTool(tool_name)
+#     if tool_function:
+#         observation = tool_function(tool_input)
+#         print("--- 观察 (Observation) ---")
+#         print(observation)
+#     else:
+#         print(f"错误:未找到名为 '{tool_name}' 的工具。")
+
+
 
 # --- 工具初始化与使用示例 ---
 if __name__ == '__main__':
-    # 1. 初始化工具执行器
-    toolExecutor = ToolExecutor()
+    llm = HelloAgentsLLM()
+    tool_executor = ToolExecutor()
+    search_desc = "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中找不到的信息时，应使用此工具。"
+    tool_executor.registerTool("Search", search_desc, search)
+    agent = ReActAgent(llm_client=llm, tool_executor=tool_executor)
+    # question = "华为最新的手机是哪一款？它的主要卖点是什么？"
+    question = "今日沪深300涨幅情况？"
+    agent.run(question)
 
-    # 2. 注册我们的实战搜索工具
-    search_description = "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中找不到的信息时，应使用此工具。"
-    toolExecutor.registerTool("Search", search_description, search)
-    
-    # 3. 打印可用的工具
-    print("\n--- 可用的工具 ---")
-    print(toolExecutor.getAvailableTools())
-
-    # 4. 智能体的Action调用，这次我们问一个实时性的问题
-    print("\n--- 执行 Action: Search['英伟达最新的GPU型号是什么'] ---")
-    tool_name = "Search"
-    tool_input = "英伟达最新的GPU型号是什么"
-
-    tool_function = toolExecutor.getTool(tool_name)
-    if tool_function:
-        observation = tool_function(tool_input)
-        print("--- 观察 (Observation) ---")
-        print(observation)
-    else:
-        print(f"错误:未找到名为 '{tool_name}' 的工具。")
